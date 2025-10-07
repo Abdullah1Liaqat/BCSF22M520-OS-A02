17eddf9ab3f81ded0cf8cc4b6933a571eb93a660
Abdullah1Liaqat <bcsf22m520@pucit.edu.pk>
2025-10-07 22:52:57 +0500

    feat: add modification time sorting (-t) for final version v2.6.0

--- src/ls-v1.0.0.c
+++ src/ls-v1.0.0.c
@@ -1,11 +1,12 @@
 /*
- * Programming Assignment 02: lsv2.5.0
+ * Programming Assignment 02: lsv2.6.0
  * Features:
  *   - Normal listing
  *   - Recursive (-R)
  *   - Show hidden files (-a)
  *   - Alphabetical sorting (default)
  *   - Sort by file size (-S)
+ *   - Sort by modification time (-t)
  */
 
 #include <stdio.h>
@@ -15,41 +16,46 @@
 #include <string.h>
 #include <errno.h>
 #include <sys/stat.h>
+#include <time.h>
 
 extern int errno;
 
 typedef struct {
     char name[256];
     off_t size;
+    time_t mtime;
 } FileInfo;
 
-void do_ls(const char *dir, int recursive, int show_all, int sort_by_size);
-void list_dir(const char *dir, int recursive, int show_all, int sort_by_size);
+void do_ls(const char *dir, int recursive, int show_all, int sort_by_size, int sort_by_time);
+void list_dir(const char *dir, int recursive, int show_all, int sort_by_size, int sort_by_time);
 int compare_alpha(const void *a, const void *b);
 int compare_size(const void *a, const void *b);
+int compare_time(const void *a, const void *b);
 
 int main(int argc, char const *argv[])
 {
     int recursive = 0;
     int show_all = 0;
     int sort_by_size = 0;
+    int sort_by_time = 0;
     int start_index = 1;
 
-    // Parse flags (-a, -R, -S)
+    // Parse all flags
     for (int i = 1; i < argc; i++) {
         if (argv[i][0] == '-') {
             if (strchr(argv[i], 'R')) recursive = 1;
             if (strchr(argv[i], 'a')) show_all = 1;
             if (strchr(argv[i], 'S')) sort_by_size = 1;
+            if (strchr(argv[i], 't')) sort_by_time = 1;
             start_index++;
         } else break;
     }
 
     if (argc == 1 || start_index == argc) {
-        list_dir(".", recursive, show_all, sort_by_size);
+        list_dir(".", recursive, show_all, sort_by_size, sort_by_time);
     } else {
         for (int i = start_index; i < argc; i++) {
-            list_dir(argv[i], recursive, show_all, sort_by_size);
+            list_dir(argv[i], recursive, show_all, sort_by_size, sort_by_time);
             if (i < argc - 1) puts("");
         }
     }
@@ -57,10 +63,10 @@ int main(int argc, char const *argv[])
     return 0;
 }
 
-void list_dir(const char *dir, int recursive, int show_all, int sort_by_size)
+void list_dir(const char *dir, int recursive, int show_all, int sort_by_size, int sort_by_time)
 {
     printf("Directory listing of %s:\n", dir);
-    do_ls(dir, recursive, show_all, sort_by_size);
+    do_ls(dir, recursive, show_all, sort_by_size, sort_by_time);
     puts("");
 }
 
@@ -80,7 +86,16 @@ int compare_size(const void *a, const void *b)
     else return strcmp(fa->name, fb->name);
 }
 
-void do_ls(const char *dir, int recursive, int show_all, int sort_by_size)
+int compare_time(const void *a, const void *b)
+{
+    const FileInfo *fa = (const FileInfo *)a;
+    const FileInfo *fb = (const FileInfo *)b;
+    if (fb->mtime > fa->mtime) return 1;
+    else if (fb->mtime < fa->mtime) return -1;
+    else return strcmp(fa->name, fb->name);
+}
+
+void do_ls(const char *dir, int recursive, int show_all, int sort_by_size, int sort_by_time)
 {
     struct dirent *entry;
     DIR *dp = opendir(dir);
@@ -104,6 +119,7 @@ void do_ls(const char *dir, int recursive, int show_all, int sort_by_size)
         if (stat(path, &st) == 0) {
             strncpy(files[count].name, entry->d_name, sizeof(files[count].name) - 1);
             files[count].size = st.st_size;
+            files[count].mtime = st.st_mtime;
             count++;
         }
     }
@@ -113,15 +129,20 @@ void do_ls(const char *dir, int recursive, int show_all, int sort_by_size)
 
     closedir(dp);
 
-    // Sort
-    if (sort_by_size)
+    // Sort by time > size > name priority
+    if (sort_by_time)
+        qsort(files, count, sizeof(FileInfo), compare_time);
+    else if (sort_by_size)
         qsort(files, count, sizeof(FileInfo), compare_size);
     else
         qsort(files, count, sizeof(FileInfo), compare_alpha);
 
     // Print
     for (int i = 0; i < count; i++) {
-        printf("%-30s %ld bytes\n", files[i].name, (long)files[i].size);
+        struct tm *tm_info = localtime(&files[i].mtime);
+        char timebuf[64];
+        strftime(timebuf, sizeof(timebuf), "%b %d %H:%M", tm_info);
+        printf("%-30s %8ld bytes   %s\n", files[i].name, (long)files[i].size, timebuf);
     }
 
     // Recursive
@@ -136,7 +157,7 @@ void do_ls(const char *dir, int recursive, int show_all, int sort_by_size)
                 && strcmp(files[i].name, "..") != 0
                 && strcmp(files[i].name, ".git") != 0) {
                 printf("\n%s:\n", path);
-                do_ls(path, recursive, show_all, sort_by_size);
+                do_ls(path, recursive, show_all, sort_by_size, sort_by_time);
             }
         }
     }
